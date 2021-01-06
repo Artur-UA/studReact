7016113803b03fd45479e88b995371cec2b8ef8a
Artur <kovalskartur@yahoo.com>
2021-01-06 19:15:06 +0200

    'six'

--- src/components/Friends/Friends.js
+++ src/components/Friends/Friends.js
@@ -1,13 +1,20 @@
 import React from 'react'
+import * as axios from 'axios'
 
 const Friends = (props) => {
 
     if (props.info.length === 0 ) {
-    props.addFriendAC([
-        {id: 1, friend: true, fullName: 'Remi Iren', location: {city: 'Kyiv', country: 'Ukraine'}, img: 'https://lh3.googleusercontent.com/proxy/g8DFkSBR70g-sCJu1gLgn85wajfb9xC11QidsvQ-CEmsidggOTxn0M9dmtg4CRzcPREDPYWXjJzFoEruBpSJbqAQRA4rEtZmplN0TA3QX6l1wF9qyoom7otEIPOdOvHahVTEWYG4M4-kXWg'},
-        {id: 2, friend: false, fullName: 'Sofia Loren', location: {city: 'Dnipro', country: 'Ukraine'}, img: 'https://lh3.googleusercontent.com/proxy/g8DFkSBR70g-sCJu1gLgn85wajfb9xC11QidsvQ-CEmsidggOTxn0M9dmtg4CRzcPREDPYWXjJzFoEruBpSJbqAQRA4rEtZmplN0TA3QX6l1wF9qyoom7otEIPOdOvHahVTEWYG4M4-kXWg'},
-        {id: 3, friend: true, fullName: 'Viktor Popov', location: {city: 'Kharkiv', country: 'Ukraine'}, img: 'https://lh3.googleusercontent.com/proxy/g8DFkSBR70g-sCJu1gLgn85wajfb9xC11QidsvQ-CEmsidggOTxn0M9dmtg4CRzcPREDPYWXjJzFoEruBpSJbqAQRA4rEtZmplN0TA3QX6l1wF9qyoom7otEIPOdOvHahVTEWYG4M4-kXWg'}
-    ])
+        /* props.addFriendAC([
+            {id: 1, friend: true, fullName: 'Remi Iren', location: {city: 'Kyiv', country: 'Ukraine'}, img: 'https://lh3.googleusercontent.com/proxy/g8DFkSBR70g-sCJu1gLgn85wajfb9xC11QidsvQ-CEmsidggOTxn0M9dmtg4CRzcPREDPYWXjJzFoEruBpSJbqAQRA4rEtZmplN0TA3QX6l1wF9qyoom7otEIPOdOvHahVTEWYG4M4-kXWg'},
+            {id: 2, friend: false, fullName: 'Sofia Loren', location: {city: 'Dnipro', country: 'Ukraine'}, img: 'https://lh3.googleusercontent.com/proxy/g8DFkSBR70g-sCJu1gLgn85wajfb9xC11QidsvQ-CEmsidggOTxn0M9dmtg4CRzcPREDPYWXjJzFoEruBpSJbqAQRA4rEtZmplN0TA3QX6l1wF9qyoom7otEIPOdOvHahVTEWYG4M4-kXWg'},
+            {id: 3, friend: true, fullName: 'Viktor Popov', location: {city: 'Kharkiv', country: 'Ukraine'}, img: 'https://lh3.googleusercontent.com/proxy/g8DFkSBR70g-sCJu1gLgn85wajfb9xC11QidsvQ-CEmsidggOTxn0M9dmtg4CRzcPREDPYWXjJzFoEruBpSJbqAQRA4rEtZmplN0TA3QX6l1wF9qyoom7otEIPOdOvHahVTEWYG4M4-kXWg'}
+        ]) */
+debugger;
+        axios.get('https://social-network.samuraijs.com/api/1.0/users')
+            .then(response => {
+                console.log(response)
+                props.addFriendAC(response.data.items)
+            })
     }
 
     console.log(props);
@@ -15,9 +22,9 @@ const Friends = (props) => {
         <div>
         Friendau
         { props.info.map( i => <div key={i.id}>
-                <div>{i.fullName}</div><span>{i.friend}</span>
-                <div><strong>{i.location.city}</strong>{i.location.country}</div>
-                <img src={i.img} alt="Ель-Шевченко"/>
+                <div>{i.name}</div><span>{i.followed}</span>
+                <div><strong>i.location.city</strong>i.location.country</div> 
+                <img src={i.photos.small || "https://ihde.tsu.ru/wp-content/uploads/2017/10/no-ava-300x300.png"} alt="Ель-Шевченко"/>
                 <div>
                     
                         {i.friend
